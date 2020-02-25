f197ec7af16ae1188e19ad9aebcaa009ed7e3f33
malin798 <malin.marie@hotmail.se>
2020-02-25 13:52:28 +0100

    bug

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiKey = "6bd2892ddab2e2361c20e0ea6bbd659c";
 const cityId = 282; //Las Vegas
-const cuisineId = 182; //Breakfast
+const cuisineId = 25; //Breakfast
 //https://developers.zomato.com/api/v2.1/search?entity_id=${cityId}&entity_type=city&cuisines=${cuisineId}
 let maindata = ""
 let activate = ""
@@ -21,20 +21,7 @@ fetch(
     console.log("API response:", apiData);
 
     maindata = apiData
-
-    //json.restaurants.forEach(item => {
-    //console.log(item.restaurant.name);
-    //});
-
     apiData.restaurants.forEach(item => {
-      //console.log(item.restaurant.name);
-      //console.log(item.restaurant.user_rating.aggregate_rating);
-      //console.log(item.restaurant.user_rating.rating_text);
-      //console.log(item.restaurant.highlights[0]);
-      //console.log(item.restaurant.average_cost_for_two);
-      //console.log(item.restaurant.location.address);
-
-      //console.log(item.restaurant.photos[0].photo.thumb_url);
 
 
       container.innerHTML += `<p><span class="resLabel">Restaurant:</span> ${
@@ -58,70 +45,44 @@ fetch(
 const container = document.getElementById("resContainer");
 
 
-
 document.getElementById("deliveryDropdown").addEventListener("change", () => revealFilter(deliveryDropdown.value));
 
 revealFilter = (value) => {
 
   if (value === "Homedelivery") {
     filterDelivery(maindata);
+
   }
 
   else if (value === "Online-booking") {
     filterTableBooking(maindata);
+
   };
 
 };
 
 const filterDelivery = (maindata) => {
-  let hasHomeDelivery = {};
-  hasHomeDelivery = maindata.restaurants.filter(resto => resto.restaurant.has_online_delivery !== "0");
-  console.log(hasHomeDelivery);
+  let hasHomeDelivery = [];
 
-  //activate = hasHomeDelivery;
+  maindata.restaurants.forEach(item => {
+    if (item.restaurant.has_online_delivery !== 0) {
+      hasHomeDelivery.push(item)
+    }
+  })
 
-  //showNewList()
-
+  return hasHomeDelivery
 };
 
+
 const filterTableBooking = (maindata) => {
-  let hasTableBooking = {};
-  hasTableBooking = maindata.restaurants.filter(resto => resto.restaurant.has_table_booking !== "0");
-  console.log(hasTableBooking);
+  let hasTableBooking = [];
 
-  hasTableBooking.forEach((restaurant) => {
-    console.log(restaurant)
-    container.innerHTML = "";
-    container.innerHTML +=
-      `<p><span class="resLabel">Restaurant:</span>
-      ${restaurant.name}. <br/><span class="resLabel">Rating:</span> ${restaurant.user_rating.aggregate_rating}.
-       ${restaurant.user_rating.rating_text}. <br/><span class="resLabel">Info:</span>
-       ${restaurant.highlights[0]}. <br/><span class="resLabel">Snittkostnad (2 pers):</span>
-       ${restaurant.average_cost_for_two}.<br/><span class="resLabel">Address:</span>
-       ${restaurant.location.address}. <img src="${restaurant.thumb}"/></p>`;
+  maindata.restaurants.forEach(item => {
+    if (item.restaurant.has_table_booking !== 0) {
+      hasTableBooking.push(item)
+    }
 
-  });
+  })
 
-
-  //activate = hasTableBooking;
-
-  //showNewList()
-}
-
-
-/*const showNewList = (activate) => {
-
-  activate.forEach((restaurant) => {
-    console.log(restaurant)
-    container.innerHTML = "";
-    container.innerHTML +=
-      `<p><span class="resLabel">Restaurant:</span>
-      ${restaurant.name}. <br/><span class="resLabel">Rating:</span> ${restaurant.user_rating.aggregate_rating}.
-       ${restaurant.user_rating.rating_text}. <br/><span class="resLabel">Info:</span>
-       ${restaurant.highlights[0]}. <br/><span class="resLabel">Snittkostnad (2 pers):</span>
-       ${restaurant.average_cost_for_two}.<br/><span class="resLabel">Address:</span>
-       ${restaurant.location.address}. <img src="${restaurant.thumb}"/></p>`;
-
-  });
-
-}; */
+  return hasTableBooking
+};
